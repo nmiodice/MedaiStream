365cc947e12fec825407e9b9a906dd89951b57b6
Nick Iodice <nmiodice@gmail.com>
2016-07-07 00:02:02 -0400

    fixed HTML plain text view

--- client/js/components/preview-pane/PlainText.react.js
+++ client/js/components/preview-pane/PlainText.react.js
@@ -10,13 +10,29 @@ var PlainText = React.createClass({
             var uri = UriUtils.fileToURI(this.props.file);
             UIUtils.addLoader();
 
-            $('#text-container').load(
-                uri,
-                null,
-                function() {
+            //$('#text-container').load(
+            //    uri,
+            //    null,
+            //    function(txt) {
+            //        alert(txt);
+            //        UIUtils.removeLoader();
+            //    }.bind(this)
+            //);
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (xhttp.readyState == 4 && xhttp.status == 200) {
                     UIUtils.removeLoader();
-                }.bind(this)
-            );
+                    var text = xhttp.responseText;
+                    text = text.replace(/&/g, '&amp;');
+                    text = text.replace(/</g, '&lt;');
+                    text = text.replace(/>/g, '&gt;');
+
+                    $('#text-container')[0].innerHTML = text;
+                }
+            };
+            xhttp.open("GET", uri, true);
+            xhttp.send();
         }
     },
 
