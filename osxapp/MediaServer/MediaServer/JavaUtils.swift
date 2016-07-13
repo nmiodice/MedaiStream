8dbb27676430ff9a2ae176f5a850eb0786529979
Nick Iodice <nmiodice@gmail.com>
2016-07-13 10:57:14 -0400

    added run state to osx app. also look for server process on startup

--- osxapp/MediaServer/MediaServer/JavaUtils.swift
+++ osxapp/MediaServer/MediaServer/JavaUtils.swift
@@ -11,15 +11,57 @@ import Foundation
 class JavaUtils {
     
     static let NO_PID: Int32 = -1
-    
+    static let JAR_NAME: String = "mediaserver-1.0-SNAPSHOT.jar"
     var javaPID: Int32 = JavaUtils.NO_PID
+    
+    init () {
+        var output : [String] = []
+        
+        let task = NSTask()
+        task.launchPath = "/bin/ps"
+        task.arguments = ["-ef"]
+        
+        let outpipe = NSPipe()
+        task.standardOutput = outpipe
+        task.launch()
+        
+        let outdata = outpipe.fileHandleForReading.readDataToEndOfFile()
+        if var string = String.fromCString(UnsafePointer(outdata.bytes)) {
+            string = string.stringByTrimmingCharactersInSet(NSCharacterSet.newlineCharacterSet())
+            output = string.componentsSeparatedByString("\n")
+            
+            for line in output {
+                if line.rangeOfString(JavaUtils.JAR_NAME) != nil {
+                    var parts : [String] = line.componentsSeparatedByCharactersInSet(.whitespaceAndNewlineCharacterSet())
+                    
+                    var count = 0
+                    for p in parts {
+                        var val = p.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).toInt()
+
+                        if val != nil {
+                            count++
+                        }
+                        
+                        if count == 2 {
+                            self.javaPID = Int32(val!);
+                            println("found existing server, PID = \(self.javaPID)");
+                            return
+                        }
+                    }
+                }
+            }
+        }
+        
+        println("did not find server running");
+    }
+    
     
     func startServer(sharedDirectory: String) -> Bool {
         if isRunning() {
             return true
         }
         
-        let jarPath = "/Users/nmiodice/Dropbox/dev/projects/mediaserver/server/target/mediaserver-1.0-SNAPSHOT.jar"
+        let jarPath = "/Users/nmiodice/Dropbox/dev/projects/mediaserver/server/target/" + JavaUtils.JAR_NAME
         
         let task = NSTask()
         task.launchPath = "/usr/bin/java"
@@ -48,12 +90,11 @@ class JavaUtils {
     }
     
     func isRunning() -> Bool {
-        println(self.javaPID);
-        println(JavaUtils.NO_PID);
         return self.javaPID != JavaUtils.NO_PID
     }
     
     deinit {
+        println("deinit")
         if isRunning() {
             stopServer()
         }
--- osxapp/MediaServer/MediaServer/ViewController.swift
+++ osxapp/MediaServer/MediaServer/ViewController.swift
@@ -15,19 +15,19 @@ class ViewController: NSViewController {
     
     @IBOutlet weak var tableView: NSTableView!
     
-    let javaUtil: JavaUtils = JavaUtils()
+    @IBOutlet weak var serverStatus: NSTextField!
     
+    let javaUtil: JavaUtils = JavaUtils()
     
     var fileList: [[String]] = [[String]]();
     
-    var serverRunning: Bool = false;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.setDelegate(self)
         tableView.setDataSource(self)
         reloadTable()
-        reloadServerButton()
+        reloadServerInfo()
     }
 
     override var representedObject: AnyObject? {
@@ -36,25 +36,25 @@ class ViewController: NSViewController {
         }
     }
     
-    func reloadServerButton() {
-        serverButton!.title = serverRunning ? "Stop Server" : "Start Server"
+    func reloadServerInfo() {
+        if javaUtil.isRunning() {
+            serverButton!.title = "Stop Server"
+            serverStatus!.textColor = NSColor.greenColor()
+            serverStatus!.stringValue = "running"
+        } else {
+            serverButton!.title = "Run Server"
+            serverStatus!.textColor = NSColor.redColor()
+            serverStatus!.stringValue = "stopped"
+        }
     }
     
     @IBAction func onServerButtonClick(sender: NSButtonCell) {
-        if serverRunning {
-            if javaUtil.stopServer() {
-                serverRunning = false
-            } else {
-                serverRunning = true
-            }
+        if javaUtil.isRunning() {
+            javaUtil.stopServer()
         } else {
-            if javaUtil.startServer(FileUtils.getShareFolderPath()) {
-                serverRunning = true
-            } else {
-                serverRunning = false
-            }
+            javaUtil.startServer(FileUtils.getShareFolderPath())
         }
-        reloadServerButton();
+        reloadServerInfo();
     }
     
     func reloadTable() {
